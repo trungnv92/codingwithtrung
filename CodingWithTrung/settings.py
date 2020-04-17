d46a2a911ec26d7c53716c6bae9903ca09586784
trungcuibap <trungnv.vnimm@gmail.com>
2020-04-17 09:01:25 +0700

    edit api blog

--- CodingWithTrung/settings.py
+++ CodingWithTrung/settings.py
@@ -53,7 +53,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1,
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 
 from account.models import Account
 from blog.models import BlogPost
@@ -86,3 +89,10 @@ def api_create_blog_view(request):
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ApiBlogListView(ListAPIView):
+	queryset = BlogPost.objects.all()
+	serializer_class = BlogPostSerializer
+	authentication_class = (TokenAuthentication,)
+	permission_classes = (IsAuthenticated,)
+	pagination_class = PageNumberPagination
